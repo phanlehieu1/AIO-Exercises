e44392dcc206fecec9d03786125748929510003e
phanlehieu <phanlehieu1@gmail.com>
2024-07-01 21:28:41 +0700

    Exercises Wee4 Streamlit Project

--- Module1_Week3_Exercises/ex2_ward_and_people.py
+++ Module1_Week3_Exercises/ex2_ward_and_people.py
@@ -64,18 +64,43 @@ class Ward:
         return sum(teacher.yob for teacher in teachers) / len(teachers)
 
 
+# if __name__ == "__main__":
+#     student1 = Student(name="studentA", yob=2010, grade="7")
+#     student1.describe()
+
+#     teacher1 = Teacher(name="teacherA", yob=1969, subject="Math")
+#     teacher1.describe()
+
+#     doctor1 = Doctor(name="doctorA", yob=1945, specialist="Endocrinologists")
+#     doctor1.describe()
+
+#     print()
+#     teacher2 = Teacher(name="teacherB", yob=1995, subject="History")
+#     doctor2 = Doctor(name="doctorB", yob=1975, specialist="Cardiologists")
+#     ward1 = Ward(name="Ward1")
+#     ward1.add_person(student1)
+#     ward1.add_person(teacher1)
+#     ward1.add_person(teacher2)
+#     ward1.add_person(doctor1)
+#     ward1.add_person(doctor2)
+#     ward1.describe()
+
+#     print(f"\nNumber of doctors: {ward1.count_doctor()}")
+
+#     print("\nAfter sorting Age of Ward1 people")
+#     ward1.sort_age()
+#     ward1.describe()
+
+#     print(f"\nAverage year of birth (teachers): {ward1.compute_average()}")
+
+
 if __name__ == "__main__":
+    ward1 = Ward(name="Ward1")
     student1 = Student(name="studentA", yob=2010, grade="7")
-    student1.describe()
-
     teacher1 = Teacher(name="teacherA", yob=1969, subject="Math")
-    teacher1.describe()
-
-    doctor1 = Doctor(name="doctorA", yob=1945, specialist="Endocrinologists")
-    doctor1.describe()
-
-    print()
     teacher2 = Teacher(name="teacherB", yob=1995, subject="History")
+    doctor1 = Doctor(name="doctorA", yob=1945, specialist="Endocrinologists")
+    assert ward1.count_doctor() == 1
     doctor2 = Doctor(name="doctorB", yob=1975, specialist="Cardiologists")
     ward1 = Ward(name="Ward1")
     ward1.add_person(student1)
@@ -83,12 +108,4 @@ if __name__ == "__main__":
     ward1.add_person(teacher2)
     ward1.add_person(doctor1)
     ward1.add_person(doctor2)
-    ward1.describe()
-
-    print(f"\nNumber of doctors: {ward1.count_doctor()}")
-
-    print("\nAfter sorting Age of Ward1 people")
-    ward1.sort_age()
-    ward1.describe()
-
-    print(f"\nAverage year of birth (teachers): {ward1.compute_average()}")
+    ward1.count_doctor()
